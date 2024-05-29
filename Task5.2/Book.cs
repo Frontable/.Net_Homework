4c994bc4f06953c7de22bdb330bbf093f5b6180a
Frontabl3 <Frontabl3@gmail.com>
2024-05-29 20:22:39 +0300

    Task 5.2

---
+++ Task5.2/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Book
+{
+	public string Title { get; private set; }
+	public DateTime? PublicationDate { get; private set; }
+	public HashSet<string> Authors { get; private set; }
+
+	public Book(string title, DateTime? publicationDate, IEnumerable<string> authors)
+	{
+		if (string.IsNullOrWhiteSpace(title))
+		{
+			throw new ArgumentException("Title cannot be null or empty.", nameof(title));
+		}
+
+		Title = title;
+		PublicationDate = publicationDate;
+		Authors = new HashSet<string>(authors ?? new List<string>());
+	}
+
+	public override string ToString()
+	{
+		return $"{Title} ({PublicationDate?.ToString("yyyy-MM-dd") ?? "No Date"}) by {string.Join(", ", Authors)}";
+	}
+}
---
+++ Task5.2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+	public static void Main()
+	{
+		
+		Catalog catalog = new Catalog();
+
+		
+		catalog.AddBook("123-4-56-789012-3", new Book("Book One", new DateTime(2020, 1, 1), new[] { "Author A", "Author B" }));
+		catalog.AddBook("1234567890123", new Book("Book Two", new DateTime(2019, 5, 15), new[] { "Author A" }));
+		catalog.AddBook("987-6-54-321098-7", new Book("Book Three", null, new[] { "Author C" }));
+
+		
+		var sortedTitles = catalog.GetSortedBookTitles();
+		Console.WriteLine("Sorted Book Titles:");
+		foreach (var title in sortedTitles)
+		{
+			Console.WriteLine(title);
+		}
+
+		
+		var booksByAuthorA = catalog.GetBooksByAuthor("Author A");
+		Console.WriteLine("\nBooks by Author A:");
+		foreach (var book in booksByAuthorA)
+		{
+			Console.WriteLine(book);
+		}
+
+		
+		var authorBookCounts = catalog.GetAuthorBookCounts();
+		Console.WriteLine("\nAuthor Book Counts:");
+		foreach (var (Author, Count) in authorBookCounts)
+		{
+			Console.WriteLine($"{Author} - {Count} book(s)");
+		}
+
+		
+		var bookByIsbn = catalog.GetBook("123-4-56-789012-3");
+		Console.WriteLine($"\nRetrieved Book by ISBN 123-4-56-789012-3: {bookByIsbn}");
+
+		bookByIsbn = catalog.GetBook("1234567890123");
+		Console.WriteLine($"\nRetrieved Book by ISBN 1234567890123: {bookByIsbn}");
+	}
+}
